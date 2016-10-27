5231f3020e459265d1bd8f50216642e7e6e859fd
Erica Nafziger and Eric Raetz <eric@>
2016-10-27 16:54:49 -0700

    added up to length 6; returns undefined for any empty space which will need to be fixed; :

--- js/script.js
+++ js/script.js
@@ -2,13 +2,21 @@ var coder = function(input) {
 	debugger;
 	var removed = [];
 	var removed2 = [];
+	// var removed3 = [];
 	input = input.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
 	var length = input.length;
 	if (length <= 4) {
 		var input = input.split("");
 		removed = input.splice(0, 2);
 		removed2 = input.splice(0, 2);
-		return removed2;
+		var output = removed[0] + removed2[0] + removed[1] + removed2[1];
+		output = output.replace(/[^A-Za-z0-9]/g, '');
+		return output;
+	} else if (length <= 6) {
+		var input = input.split("");
+		removed = input.splice(0, 3);
+		removed2 = input.splice(0, 3);
+		return removed[0] + removed2[0] + removed[1] + removed2[1] + removed[2] + removed2[2];
 	}
 }
 
